74b13248941fe70fd44513bbd1277aad0025cc59
Sudhi Ranjan Gupta <sudhigupta190@gmail.com>
2023-12-13 10:35:10 +0530

    --issues null not return from --non-nullable

--- src/graphql/Resolvers/User/userType.ts
+++ src/graphql/Resolvers/User/userType.ts
@@ -1,8 +1,23 @@
 export interface RegisterUserInput {
   email: string;
   password: string;
-  street?: string ;
-  zipcode?: string;
-  houseNumber?: string;
-  location?: string;
+  street: string;
+  zipcode: string;
+  houseNumber: string;
+  location: string;
+}
+
+export interface Address {
+  id: string;
+  street: string;
+  zipcode: string;
+  houseNumber: string;
+  location: string;
+}
+
+export interface RegisterUserResponse {
+  id: string;
+  email: string;
+  password: string;
+  address?: Address;
 }
